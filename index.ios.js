31a8b8aae37f7904db69341831ef3a795f89c82c
Ahmad Hassan <er.ahmad.hassan@gmail.com>
2017-08-03 16:44:22 +0530

    Search Page Design

--- index.ios.js
+++ index.ios.js
@@ -9,18 +9,29 @@
 'use strict';
 
 import React, {Component} from 'react';
+import SearchPage from './SearchPage';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS
 } from 'react-native';
 
-export default class PropertyFinder extends Component {
+
+class PropertyFinder extends Component {
   render() {
-    return <Text style={styles.description}>Search for houses to buy!</Text>;
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    )
   }
 }
+
 
 const styles = StyleSheet.create({
   description: {
@@ -28,6 +39,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#656565',
     marginTop: 65,
+  },
+  container: {
+    flex: 1,
   },
 });
 
